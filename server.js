d345f6b78b7d4cc678e20d6d13f4ac4af0d3b373
rambabutanneru <ram.javatechie@gmail.com>
2016-07-01 15:22:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.set('port', process.env.PORT || 1440);
+var server = app.listen(app.get('port'), function() {
+	console.log("Edu Cources App runing on PORT:"+app.get('port'));	
+});
